65997fb190c556cd9065e2d9cf585dbc824f4fb1
Fabio Tomat <f.t.public@gmail.com>
2025-03-06 14:34:49 +0100

    fix values > 999,99

--- cedoliner.py
+++ cedoliner.py
@@ -388,7 +388,10 @@ for root, dirs, files in os.walk(cartella_pdf):
     for pagina, mese, parola,valore,descrizione in risultati_ordinati:
         if start_row_for_year is None:
             start_row_for_year = ws.max_row + 1  # Set the starting row for the current year
-        ws.append([mese, str(pagina), parola,descrizione, float(valore.replace(",","."))])
+        if len(valore)>6:
+            valore.replace(".","")
+        val = float(valore.replace(",","."))
+        ws.append([mese, str(pagina), parola,descrizione, val])
         cell = ws.cell(row=ws.max_row,column=5)
         if str(valore).startswith("-"):
             cell.fill = red_fill
